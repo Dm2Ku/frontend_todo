e9de3effe64d8d2c46f147395e1e4cb85778e76a
Kukulin <dkukulin@gmail.com>
2022-10-11 11:28:10 +0200

    first

---
+++ src/app/service/hardcoded-authentication.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HardcodedAuthenticationService {
+
+  constructor() {
+  }
+
+  authenticate(username, password) {
+    // console.log('before ' + this.isUserLoggedIn());
+    if (username === 'qwer' && password === 'dummy') {
+      sessionStorage.setItem('authenticateUser', username);
+      // console.log('after ' + this.isUserLoggedIn());
+      return true;
+    }
+    return false;
+  }
+
+  isUserLoggedIn() {
+    let user: string;
+    user = sessionStorage.getItem('authenticateUser');
+    return !(user === null);
+  }
+
+  logout() {
+    sessionStorage.removeItem('authenticateUser');
+  }
+
+}
